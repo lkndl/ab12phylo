1d8cb2de2d25d268ead5400ee10791f5e117b29f
Leo Kaindl <leo.kaindl@tum.de>
2020-06-10 21:38:24 +0200

    0.1b.3 for whitelisting: added motif exclusion capability and choice of paths or sample IDs. not using string replacing anymore, but lxml.etree traversal mostly, except for svg markup. typos in table.

--- ab12phylo/cgi-bin/ab12phylo.py
+++ ab12phylo/cgi-bin/ab12phylo.py
@@ -11,6 +11,8 @@ import re
 import toytree
 
 from os import path, makedirs
+from lxml import etree, html
+from lxml.html import builder as E
 from Bio import SeqIO
 from dendropy import DnaCharacterMatrix
 from dendropy.calculate import popgenstat
@@ -25,12 +27,49 @@ call = form['submit'].value
 motifs = form['motifs'].value
 msa = form['msa_path'].value
 pickle_file = form['pickle'].value
+try:
+    exclude = form['exclude'].value
+except KeyError:
+    exclude = ''
+as_files = True if form['out_type'].value == 'files' else False  # or exclude != '' else False
 
-# read in original page
-with open('result.html', 'r') as html_fh:
-    html = html_fh.read()
 
-# read toytree.Toytree.ToyTree from pickle file
+def _process(leaves, ex_motifs, as_files):
+    """
+    Raise error if no leaves found.
+    If exclusion patterns were specified, remove matching motifs.
+    If requested or samples were excluded, return file paths.
+    :return:
+    """
+    print('Content-type: text/html\n\n')
+    if len(leaves) == 0:
+        raise ValueError('no matches')
+
+    excluded = list()
+    if ex_motifs != '':
+        # compile regex from patterns
+        queries = ['.*' + re.escape(word.strip()) + '.*' for word in ex_motifs.split(',')]
+        regex = re.compile(r'|'.join(queries))
+
+        for ex in list(filter(regex.match, leaves)):
+            excluded.append(ex)
+            leaves.remove(ex)
+        # as_files = True if len(excluded) > 0 or as_files else False
+
+    # cut to sample IDs
+    leaves = [leaf.split(' ')[0] for leaf in leaves]
+    if not as_files:
+        return leaves, excluded, None
+
+    # translate to file paths
+    with open('metadata.tsv', 'r') as tsv:
+        sam_to_file = {leaves.index(sample): _file for sample, _file
+                       in [line.split('\t')[1:3] for line in tsv.readlines()] if sample in leaves}
+    files = [_file for sample, _file in sorted(sam_to_file.items())]
+    return leaves, excluded, files
+
+
+# read toytree from pickle file
 with open(pickle_file, 'rb') as pickle_fh:
     tree = pickle.load(pickle_fh)
 
@@ -44,18 +83,12 @@ try:
 
     if call == 'subtree':
         mrca = tree.get_mrca_idx_from_tip_labels(regex=regex)
-        # get tips and cut to sample IDs
-        leaves = [leaf.split(' ')[0] for leaf in tree.get_tip_labels(mrca)]
-        if len(leaves) == 0:
-            raise ValueError('no matches')
-        txt = '\n'.join(list(reversed(leaves)))
+        leaves, excluded, files = _process(tree.get_tip_labels(mrca), exclude, as_files)
+        txt = '\n'.join(reversed(files)) if as_files else '\n'.join(reversed(leaves))
         mrca = '%d match%s, subtree MRCA idx: %s' % (len(leaves), 'es' if len(leaves) > 1 else '', mrca)
     elif call == 'match':
-        # get tips and cut to sample IDs
-        leaves = [leaf.split(' ')[0] for leaf in filter(regex.match, tree.get_tip_labels())]
-        if len(leaves) == 0:
-            raise ValueError('no matches')
-        txt = '\n'.join(leaves)
+        leaves, excluded, files = _process(list(filter(regex.match, tree.get_tip_labels())), exclude, as_files)
+        txt = '\n'.join(files) if as_files else '\n'.join(leaves)
         mrca = '%d match%s' % (len(leaves), 'es' if len(leaves) > 1 else '')
     else:
         raise ValueError('illegal call')
@@ -66,7 +99,7 @@ except (ToytreeError, ValueError) as ex:
     leaves = []
 
 # save result in file
-_path = path.join('queries', call + '_' + motifs + '.txt')
+_path = path.join('queries', call + '_' + motifs + '_' + exclude + '.txt')
 with open(_path, 'w') as txt_fh:
     txt_fh.write(txt + '\n')
 
@@ -87,29 +120,40 @@ if len(leaves) > 1:
     except ZeroDivisionError:
         TD = float('inf')
 
-    # paste into html table
+    # write raw html table
     table = '<table border="1" class="dataframe">' \
             '<thead><tr style="text-align: justify;"><th>Segregating Sites</th><th>Nucleotide diversity π' \
-            '</th><th>Watterson\'s θ</th><th>Tajima\'s D</th><th>Haplotypes</th><tr><thead>' \
+            '</th><th>Watterson\'s θ</th><th>Tajima\'s D</th><th>Haplotypes</th></tr></thead>' \
             '<tbody><tr><td>%d</td><td>%.5f</td><td>%.5f</td><td>%.5f</td><td>%d</td></tr></tbody></table><hr/>' \
             % (S, pi, theta, TD, haplo)
 else:
     table = ''
 
+# read in original page
+ET = etree.parse('result.html', parser=etree.HTMLParser())
+port = int(ET.findall('//meta[@name="port"]')[0].get('content'))
+
+# fetch div of motif search results
+div = ET.findall('//div[@id="motif_result"]')[0]
+div.clear()
+
+# insert matches, mrca info, file link and stats into HTML
+div.append(E.PRE(E.CODE(txt)))
+div.append(E.P(mrca, E.SPAN('see ', E.A('file', href='http://localhost:%s/%s' % (port, _path)), style='float:right')))
+div.append(etree.fromstring('<div>%s</div>' % table))
+
 # edit logo and icon path
-html = html.replace('src="cgi-bin/favicon.png"', 'src="http://localhost:8000/.img/favicon.png"')
-html = html.replace('href="cgi-bin/favicon.ico"', 'href="http://localhost:8000/.img/favicon.ico"')
-html = html.replace('source <a href="metadata.tsv">.tsv</a> contains', 'source <a href="../metadata.tsv">.tsv</a> contains')
+ET.findall('//img[@id="logo"]')[0].set('src', 'http://localhost:%d/.img/favicon.png' % port)
+ET.findall('//link[@id="favicon"]')[0].set('href', 'http://localhost:%d/.img/favicon.ico' % port)
 
-# paste into HTML
-html = html.replace('<!--insert-->', '<pre id="code_box"><code id="query_result">%s</code></pre>'
-                                     '<p>%s<span style="float:right">see '
-                                     '<a href="http://localhost:8000/%s">file</a></span></p>%s'
-                    % (txt, mrca, _path, table))
+# re-set metadata.tsv link
+ET.findall('//a[@id="tsv_link"]')[0].set('a', '../metadata.tsv')
+# paste into input fields
+ET.findall('//input[@id="motifs"]')[0].set('value', motifs)
+ET.findall('//input[@id="exclude"]')[0].set('value', exclude)
 
-# paste into input field
-html = html.replace('<input type="text" id="motifs" name="motifs" value=""> &emsp;',
-                    '<input type="text" id="motifs" name="motifs" value="%s"> &emsp;' % motifs)
+# convert to raw text
+html = html.tostring(ET, encoding='unicode')
 
 # create a markup for selected nodes
 markup = ';font-weight: bold; font-style: italic; text-decoration: underline 0.15rem">%s'
@@ -124,5 +168,4 @@ for leaf in leaves:
     html = html.replace(no_markup % leaf, markup % leaf)
 
 print('Content-type: text/html\n\n')
-
 print(html)
