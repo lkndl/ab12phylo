0eb0a522eca9ab343c48771f4dce6200c73046e7
Leo Kaindl <leo.kaindl@tum.de>
2021-05-30 18:42:27 +0200

    v-up

--- ab12phylo/__init__.py
+++ ab12phylo/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Leo Kaindl'
 __email__ = 'leo.kaindl@tum.de'
-__version__ = '0.5.9-beta'
+__version__ = '0.5.10-beta'
 __date__ = '30 May 2021'
 __license__ = 'gpl-3.0'
 __status__ = 'Beta'
