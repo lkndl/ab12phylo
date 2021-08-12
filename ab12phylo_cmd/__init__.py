3122b758c2ebeb8b718b4ea527b4a5895e89ad8a
Leo Kaindl <leo.kaindl@tum.de>
2021-08-12 11:40:08 +0200

    0.5.20 hash, ghostscript dep

--- ab12phylo_cmd/__init__.py
+++ ab12phylo_cmd/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Leo Kaindl'
 __email__ = 'leo.kaindl@tum.de'
-__version__ = '0.5.19-beta'
-__date__ = '11 August 2021'
+__version__ = '0.5.20-beta'
+__date__ = '12 August 2021'
 __license__ = 'gpl-3.0'
 __status__ = 'Beta'
