5e92d2bb1d245ba31096bd081173a4d413ad33df
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 14:03:13 +0000

    Adding tarfile member sanitization to extractall()

--- ab12phylo_cmd/filter.py
+++ ab12phylo_cmd/filter.py
@@ -252,7 +252,26 @@ def fetch_non_python_tools(suffix, cfg, other_cfg, save_dir, log):
 
                 log.debug(f'Extracting BLAST+ ... ')
                 with tarfile.open(zf) as zo:
-                    zo.extractall(zf.parent)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(zo, zf.parent)
                 zf.unlink()
                 find(tool)
 
